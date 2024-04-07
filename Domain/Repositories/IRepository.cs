2115ecad32aa21e62b6470b1fcceb340966f1eb2
NeshGogo <rafaelaguero10@hotmail.com>
2024-04-06 22:16:48 -0400

    Repositories

---
+++ Domain/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        void Insert(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ Domain/Repositories/IRepositoryManager.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IRepositoryManager
+    {
+        IRepository<User> UserRepo { get; }
+        IRepository<Post> PostRepo { get; }
+        IRepository<Post> CommentRepo { get; }
+        IUnitOfWork UnitOfWork { get; }
+    }
+}

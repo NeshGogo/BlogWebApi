93f836b30a422c0d8b5bb1d09994c43190c581cf
NeshGogo <rafaelaguero10@hotmail.com>
2024-04-09 21:13:05 -0400

    Changes: RepositoryManager created at persistence

---
+++ Persistence/Repositories/RepositoryManager.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly Lazy<IRepository<User>> _LazyUserRepo;
+        private readonly Lazy<IRepository<Post>> _LazyPostRepo;
+        private readonly Lazy<IRepository<Comment>> _LazyCommentRepo;
+        private readonly Lazy<IUnitOfWork> _LazyUnitOfWork;
+
+        public RepositoryManager(AppDbContext dbContext)
+        {
+            _LazyUserRepo = new Lazy<IRepository<User>>(() => new Repository<User>(dbContext));
+            _LazyPostRepo = new Lazy<IRepository<Post>>(() => new Repository<Post>(dbContext));
+            _LazyCommentRepo = new Lazy<IRepository<Comment>>(() => new Repository<Comment>(dbContext));
+            _LazyUnitOfWork = new Lazy<IUnitOfWork>(() => new UnitOfWork(dbContext));
+        }
+
+        public IRepository<User> UserRepo => _LazyUserRepo.Value;
+
+        public IRepository<Post> PostRepo => _LazyPostRepo.Value;
+
+        public IRepository<Post> CommentRepo => _LazyPostRepo.Value;
+
+        public IUnitOfWork UnitOfWork => _LazyUnitOfWork.Value;
+    }
+}
